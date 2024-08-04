fda625549567b9de426ff9dc74ccd3fcab9d9a0b
Bryan Munoz <swsbmm@gmail.com>
2024-08-04 11:33:36 -0500

    feat: Creacion del componente SongList para Listar las cancion de un album

--- web/src/components/SongList.tsx
+++ web/src/components/SongList.tsx
@@ -1,5 +1,26 @@
-const SongList = () => {
-    return <></>
-}
+import { List, ListItem, ListItemIcon, ListItemText, Typography } from "@mui/material";
+import Song from "../models/Song";
+import { LibraryMusic } from "@mui/icons-material";
+import ReactAudioPlayer from "react-audio-player";
+
+type Props = { songs: Song[] };
+
+const SongList = ({ songs }: Props) => {
+    
+    return <>
+        <Typography variant="h3">Songs</Typography>
+        <List>
+            {songs.map((song) => (
+                <ListItem key={song.id}>
+                    <ListItemIcon>
+                        <LibraryMusic />
+                    </ListItemIcon>
+                    <ListItemText primary={song.name} secondary={song.album} />
+                    <ReactAudioPlayer src={song.s3Url} controls autoPlay={false} />
+                </ListItem>
+            ))}
+        </List>
+    </>;
+};
 
 export default SongList;
